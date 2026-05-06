55a448e3c2bc5637a4df0ad702a99fa16b5c3a84
EduardoCansan <eduardo.cansann@gmail.com>
2026-05-06 19:37:30 -0300

    feat: add header and footer

--- blog/helpers/url.php
+++ blog/helpers/url.php
@@ -1,4 +1,3 @@
 <?php
-    $requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($requestPath) . '/';
+    $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']. '?') . '/';
 ?>
